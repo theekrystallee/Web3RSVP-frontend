7e576ec5f7acf51600b2771b585de8fb8bbbc6a5
krystal lee <theekrystallee@gmail.com>
2022-08-10 08:16:43 -0700

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,6 @@ import { ApolloProvider } from "@apollo/client";
 import client from "../apollo-client";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
-import { ThemeProvider } from 'next-themes'
-
 
 const infuraId = process.env.NEXT_PUBLIC_INFURA_ID;
 
@@ -29,20 +27,15 @@ const wagmiClient = createClient({
 });
 
 export default function MyApp({ Component, pageProps }) {
-
   return (
-  <ThemeProvider attribute="class">
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
         <ApolloProvider client={client}>
           <Layout>
-
             <Component {...pageProps} />
-
           </Layout>
         </ApolloProvider>
       </RainbowKitProvider>
     </WagmiConfig>
-  </ThemeProvider>
   );
 }
